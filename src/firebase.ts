261bfbe2bb288e49502d85c886ba166a84ad2760
Naimuddin <naimuddins@snakconsultancy.com>
2023-07-05 16:55:12 +0530

    extra creds removed

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCWZhW9yk4DCMCg_RNUUbtCUQPdvYC5yA",
-    authDomain: "fbase-naimu.firebaseapp.com",
-    projectId: "fbase-naimu",
-    storageBucket: "fbase-naimu.appspot.com",
-    messagingSenderId: "148773338331",
-    appId: "1:148773338331:web:0f72fd89a173542280e0fe"
-  };
-
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
